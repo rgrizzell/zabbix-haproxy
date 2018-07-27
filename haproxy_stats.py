aeb9ecf9fc5b421f3de6e9f090a8e11234e3f7b5
Robert Grizzell <robert@grizzell.me>
2018-07-27 11:48:10 -0500

    Adding script structure.

--- haproxy_stats.py
+++ haproxy_stats.py
@@ -1,1 +1,21 @@
 #!/usr/bin/env python
+
+
+def discovery():
+    return True
+
+
+def get_stats():
+    return True
+
+
+def send_stats():
+    return True
+
+
+def main():
+    return True
+
+
+if __name__ == "__main__":
+    main()
